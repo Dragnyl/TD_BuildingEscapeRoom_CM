94b84aeadee28ab5ce6022e086f92757c8412cd7
Cyril Morganx <c.morganx@ludus-academie.com>
2016-10-26 16:35:19 +0200

    Initial Unreal Commit

---
+++ TD_BuildingER_CM/Source/TD_BuildingER_CM/TD_BuildingER_CM.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TD_BuildingER_CM.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TD_BuildingER_CM, "TD_BuildingER_CM" );
---
+++ TD_BuildingER_CM/Source/TD_BuildingER_CM/TD_BuildingER_CMGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "TD_BuildingER_CMGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TD_BUILDINGER_CM_API ATD_BuildingER_CMGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
